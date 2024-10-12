710b5cf7b607da21eed52dc7cfb46b8a997c0291
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-12 20:55:11 +0300

    vuetify-margin-padding-traditional-way

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,76 +1,18 @@
 <template>
   <div class="home">
-    <h1>This is Home Page</h1>
-    <h1>Example 1 Container</h1>
-    <v-container fluid>
-      <v-row>
-        <!-- grid system of columns have 12 column over all for each line (row) -->
-        <v-col cols="3">Ahmed</v-col>
-        <v-col cols="3">Mohamed</v-col>
-        <v-col cols="3">Mostafa</v-col>
-        <v-col cols="3">Sally</v-col>
-        <v-col cols="12">Test</v-col>
-      </v-row>
-    </v-container>
-    <h2>Example 2 Container</h2>
-    <v-container fluid>
-      <v-row>
-        <v-col cols="6" style="border: 1px solid">
-          <v-row>
-            <v-col cols="2" style="border: 1px solid red">test1</v-col>
-            <v-col style="border: 1px solid red">test2</v-col>
-            <v-col style="border: 1px solid red">test3</v-col>
-          </v-row>
-        </v-col>
-        <v-col cols="6" style="border: 1px solid">Mohamed</v-col>
-      </v-row>
-    </v-container>
-    <h1>Example 3 Container</h1>
-    <v-container>
-      <v-row>
-        <!-- this is for responsive design it varies based on the window screen and it's size  -->
-        <v-col
-          cols="12"
-          sm="6"
-          md="4"
-          lg="3"
-          xl="3"
-          xxl="3"
-          style="border: 1px solid"
-          >Test One</v-col
-        >
-        <v-col
-          cols="12"
-          sm="6"
-          md="4"
-          lg="3"
-          xl="3"
-          xxl="3"
-          style="border: 1px solid"
-          >Test Two</v-col
-        >
-        <v-col
-          cols="12"
-          sm="6"
-          md="4"
-          lg="3"
-          xl="3"
-          xxl="3"
-          style="border: 1px solid"
-          >Test Three</v-col
-        >
-        <v-col
-          cols="12"
-          sm="6"
-          md="4"
-          lg="3"
-          xl="3"
-          xxl="3"
-          style="border: 1px solid"
-          >Test Four</v-col
-        >
-      </v-row>
-    </v-container>
+    <h1 class="text font-weight-light">This is Home Page</h1>
+    <!-- padding and margin quantity will be in range until to 16 -->
+    <div style="border: 1px solid; width: 400px" class="m-16">Test m-16</div>
+    <div style="border: 1px solid; width: 400px" class="mt-16">Test mt-16</div>
+    <div style="border: 1px solid; width: 400px" class="mx-16">Test mx-16</div>
+    <div style="border: 1px solid; width: 400px" class="my-16">Test my-16</div>
+    <div style="border: 1px solid; width: 400px" class="mx-auto">
+      Test mx-auto
+    </div>
+    <div style="border: 1px solid" class="w-100">Test w-100</div>
+    <div style="border: 1px solid" class="w-75">Test w-75</div>
+    <div style="border: 1px solid" class="w-50">Test w-50</div>
+    <div style="border: 1px solid" class="w-25">Test w-25</div>
   </div>
 </template>
 
