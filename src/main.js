c252285cd9b676ecbace6cd4be7c0ded4b657c1f
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-12 20:38:49 +0300

    vuetify-grid-system

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+//Vuetify
+import { createVuetify } from "vuetify/lib/framework.mjs";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "vuetify/styles";
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(store).use(router).use(vuetify).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,81 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>This is Home Page</h1>
+    <h1>Example 1 Container</h1>
+    <v-container fluid>
+      <v-row>
+        <!-- grid system of columns have 12 column over all for each line (row) -->
+        <v-col cols="3">Ahmed</v-col>
+        <v-col cols="3">Mohamed</v-col>
+        <v-col cols="3">Mostafa</v-col>
+        <v-col cols="3">Sally</v-col>
+        <v-col cols="12">Test</v-col>
+      </v-row>
+    </v-container>
+    <h2>Example 2 Container</h2>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="6" style="border: 1px solid">
+          <v-row>
+            <v-col cols="2" style="border: 1px solid red">test1</v-col>
+            <v-col style="border: 1px solid red">test2</v-col>
+            <v-col style="border: 1px solid red">test3</v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="6" style="border: 1px solid">Mohamed</v-col>
+      </v-row>
+    </v-container>
+    <h1>Example 3 Container</h1>
+    <v-container>
+      <v-row>
+        <!-- this is for responsive design it varies based on the window screen and it's size  -->
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          xl="3"
+          xxl="3"
+          style="border: 1px solid"
+          >Test One</v-col
+        >
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          xl="3"
+          xxl="3"
+          style="border: 1px solid"
+          >Test Two</v-col
+        >
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          xl="3"
+          xxl="3"
+          style="border: 1px solid"
+          >Test Three</v-col
+        >
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          xl="3"
+          xxl="3"
+          style="border: 1px solid"
+          >Test Four</v-col
+        >
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
