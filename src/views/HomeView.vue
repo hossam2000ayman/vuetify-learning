c8df0c4dd0b797ff3b049031d976064f97b6d36b
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-15 00:10:32 +0300

    vuetify-v-dialog

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,57 @@
 <template>
-  <v-expansion-panels variant="inset">
-    <v-expansion-panel>
-      <v-expansion-panel-title expand-icon="mdi-account"
-        >Test Title 1</v-expansion-panel-title
-      >
-      <v-expansion-panel-text
-        >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur
-        quibusdam corporis animi amet accusantium vitae quis! Molestiae fugiat
-        eius impedit ratione esse? Incidunt, reiciendis temporibus atque placeat
-        quia beatae voluptas.</v-expansion-panel-text
-      >
-    </v-expansion-panel>
-  </v-expansion-panels>
-  <v-expansion-panels variant="popout">
-    <v-expansion-panel>
-      <v-expansion-panel-title expand-icon="mdi-plus"
-        >Test Title 2</v-expansion-panel-title
-      >
-      <v-expansion-panel-text
-        >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
-        velit dignissimos, maxime sint doloribus consectetur placeat animi fuga
-        natus dolor nesciunt doloremque ipsam cum soluta, a hic? Numquam,
-        perspiciatis nostrum!</v-expansion-panel-text
-      >
-    </v-expansion-panel>
-  </v-expansion-panels>
+  <div class="home">
+    <div class="text font-weight-light">This is home page</div>
+    <v-btn @click="showDialog1 = !showDialog1">Click Me 1 </v-btn>
+    <v-dialog width="400px" v-model="showDialog1" fullscreen>
+      <v-card>
+        <v-card-title>This is Dialog</v-card-title>
+        <v-card-text
+          >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          neque fugiat sint eligendi ex, illum voluptatem mollitia voluptatibus
+          aut beatae perspiciatis veniam odit quam dolores odio, minus porro
+          magni deserunt? .. .. .. .. .. ..</v-card-text
+        >
+        <v-card-actions>
+          <v-btn @click="showDialog1 = false" color="red">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-btn @click="showDialog2 = !showDialog2">Click Me 2 </v-btn>
+    <v-dialog v-model="showDialog2" transition="dialog-bottom-transition">
+      <v-card>
+        <v-card-title>This is Dialog</v-card-title>
+        <v-card-text
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          alias animi dolor ad iste dolorem esse, commodi eveniet, obcaecati
+          assumenda harum unde voluptatum, vel pariatur veniam hic ex enim quae!
+          .. .. .. .. .. ..</v-card-text
+        >
+        <v-card-actions>
+          <v-btn @click="showDialog2 = false" color="red">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-btn v-on:click="showDialog3 = !showDialog3">Click Me 3</v-btn>
+    <v-dialog v-model="showDialog3" persistent>
+      <v-card>
+        <v-card-title>This is Dialog</v-card-title>
+        <v-card-text
+          >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+          iusto, enim vel ipsam debitis repudiandae et. Id corrupti similique
+          dolores quod, quo corporis, dolore quidem, facilis ad itaque eos
+          debitis? .. .. .. .. .. .. .. .. .. ..</v-card-text
+        >
+        <v-card-actions>
+          <v-btn @click="showDialog3 = false" color="red">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
-
-<script></script>
+<script>
+export default {
+  data() {
+    return { showDialog1: false, showDialog2: false, showDialog3: false };
+  },
+};
+</script>
