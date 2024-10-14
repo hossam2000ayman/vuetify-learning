eaef5c8761809153150fef98c2e5f134e36b7f2a
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-15 00:19:47 +0300

    vuetify-v-sheet-and-v-menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="home">
-    <div class="text-center font-weight-light">This is Home page</div>
-    <v-card width="400" class="mx-auto" variant="outlined">
-      <v-list>
-        <v-list-item
-          v-for="(item, index) in items"
-          :key="index"
-          :value="item.value"
-          base-color="primary"
-          variant="plain"
-        >
-          <v-list-item-title class="text-center">
-            {{ item.title }}
-          </v-list-item-title>
-          <template v-slot:prepend>
-            <v-icon>{{ item.icon }}</v-icon>
-          </template>
-        </v-list-item>
-      </v-list>
-    </v-card>
+    <h1 class="text font-weight-light">This is Home Page</h1>
+    <div><p style="width: fit-content" class="bg-red py-5 px-10">Test</p></div>
+    <v-divider></v-divider>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-sheet class="bg-orange pa-10" rounded="xl" elevation="12">
+            <p>Test</p>
+          </v-sheet>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-btn id="myButton">Activator</v-btn>
+          <v-menu activator="#myButton" offset="10" location="right">
+            <v-list>
+              <v-list-item
+                v-for="(menu, index) in menus"
+                :key="index"
+                :title="menu.title"
+                :append-icon="menu.icon"
+              ></v-list-item>
+            </v-list>
+          </v-menu>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      items: [
-        { title: "Home Page", value: 1, icon: "mdi-home" },
-        { title: "Profile", value: 2, icon: "mdi-account" },
+      menus: [
+        { title: "Profile", icon: "mdi-account-outline" },
+        { title: "Home", icon: "mdi-home-outline" },
       ],
     };
   },
