52ace499e522f3b965e135ddcc62ba8c4ce7048e
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-15 00:01:19 +0300

    vuetify-expansion-panel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,30 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col v-for="card in 4" :key="card">
-        <v-card variant="outlined">
-          <v-card-title> This is title </v-card-title>
-          <v-card-subtitle>This is subtitle</v-card-subtitle>
-          <v-card-text
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nobis
-            illo vel voluptas delectus. Quae explicabo architecto exercitationem
-            quis amet. Laborum numquam, dicta minus saepe accusamus nam adipisci
-            dolores sed.</v-card-text
-          >
-          <v-divider></v-divider>
-          <v-card-actions>
-            <v-btn>
-              <v-icon>mdi-delete-outline</v-icon>
-            </v-btn>
-            <v-btn>
-              <v-icon>mdi-share-outline</v-icon>
-            </v-btn>
-          </v-card-actions>
-          <v-divider color="red" class="mb-5"></v-divider>
-          <v-divider color="green" class="mb-5" inset></v-divider>
-          <v-divider color="blue" class="mb-5" length="75%"></v-divider>
-          <v-divider color="black" class="mb-5"></v-divider>
-
-          <v-chip color="red" label variant="outlined" class="mb-5">
-            <v-icon>mdi-account</v-icon> #Andoid</v-chip
-          >
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-expansion-panels variant="inset">
+    <v-expansion-panel>
+      <v-expansion-panel-title expand-icon="mdi-account"
+        >Test Title 1</v-expansion-panel-title
+      >
+      <v-expansion-panel-text
+        >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur
+        quibusdam corporis animi amet accusantium vitae quis! Molestiae fugiat
+        eius impedit ratione esse? Incidunt, reiciendis temporibus atque placeat
+        quia beatae voluptas.</v-expansion-panel-text
+      >
+    </v-expansion-panel>
+  </v-expansion-panels>
+  <v-expansion-panels variant="popout">
+    <v-expansion-panel>
+      <v-expansion-panel-title expand-icon="mdi-plus"
+        >Test Title 2</v-expansion-panel-title
+      >
+      <v-expansion-panel-text
+        >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
+        velit dignissimos, maxime sint doloribus consectetur placeat animi fuga
+        natus dolor nesciunt doloremque ipsam cum soluta, a hic? Numquam,
+        perspiciatis nostrum!</v-expansion-panel-text
+      >
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script></script>
