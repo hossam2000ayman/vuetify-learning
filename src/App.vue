82e5cbbb5d2e08c120f03d59ca3f5a263684e922
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-17 00:06:47 +0300

    vuetify-v-footer-and-v-system-bar

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <v-layout>
     <v-app-bar
       elevation="0"
-      style="border-bottom: 1px solid rgba(0, 0, 0, 0.1)"
+      style="border-bottom: 1px solid rgba(255, 255, 255, 0.2)"
       theme="light"
-      color="white"
+      color="blue"
     >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
@@ -14,28 +14,38 @@
       <v-icon>mdi-dots-vertical</v-icon>
     </v-app-bar>
 
-    <v-app-bar elevation="2">
+    <v-app-bar elevation="2" color="blue">
       <!-- <v-breadcrumbs :items="items" divider=">>"></v-breadcrumbs> -->
-      <v-breadcrumbs :items="items" divider="-">
+      <v-breadcrumbs color="white" :items="items" divider="-">
         <!-- If you need to make override on props then you can make it through template have v-slot:<name-of-props> -->
         <!-- so this will override the divider props in the parent component (v-breadcrumbs) -->
         <template v-slot:divider>
-          <v-icon>mdi-chevron-right</v-icon>
+          <v-icon color="white">mdi-chevron-right</v-icon>
         </template>
       </v-breadcrumbs>
     </v-app-bar>
 
-    <v-bottom-navigation height="60" class="bg-cyan">
-      <v-btn>
+    <v-system-bar color="purple" elevation="10">
+      <v-icon class="mr-2">mdi-signal</v-icon>
+      <v-icon class="mr-2">mdi-wifi-strength-3</v-icon>
+      <v-icon class="mr-2">mdi-battery</v-icon>
+      <span>08:00 PM</span>
+    </v-system-bar>
+
+    <v-footer class="bg-green flex-column" elevation="20" :app="true">
+      <p style="color: white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt enim
+        iure molestiae sequi dicta eveniet quis dolorem. Minima, esse eius
+        adipisci dicta provident explicabo autem sapiente impedit ullam libero
+        sunt!
+      </p>
+      <v-container>
         <v-icon color="white">mdi-home</v-icon>
-      </v-btn>
-      <v-btn>
-        <v-icon color="white">mdi-account</v-icon>
-      </v-btn>
-      <v-btn>
-        <v-icon color="white">mdi-music</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
+        <v-icon color="white">mdi-twitter</v-icon>
+        <v-icon color="white">mdi-facebook</v-icon>
+        <v-icon color="white">mdi-instagram</v-icon>
+      </v-container>
+    </v-footer>
   </v-layout>
   <router-view />
 </template>
