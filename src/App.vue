932149db1a924e5db66122c2aac08939d8d19eef
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-16 20:08:07 +0300

    vuetify-v-tooltip-and-v-toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <v-toolbar elevation="5" class="px-5">
+    <div>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-tooltip activator="parent">Menu</v-tooltip>
+    </div>
+
+    <div>
+      <v-toolbar-title>My ToolBar Title</v-toolbar-title>
+      <v-tooltip activator="parent">My ToolBar title</v-tooltip>
+    </div>
+    <v-spacer></v-spacer>
+    <div>
+      <v-icon class="mr-3"> mdi-account </v-icon>
+      <v-tooltip activator="parent" location="bottom">Profile</v-tooltip>
+    </div>
+
+    <div>
+      <v-icon> mdi-logout </v-icon>
+      <v-tooltip activator="parent" location="bottom">Logout</v-tooltip>
+    </div>
+  </v-toolbar>
+  <!-- <nav> -->
+  <!-- <router-link to="/">Home</router-link> | -->
+  <!-- <router-link to="/about">About</router-link> -->
+  <!-- </nav> -->
   <router-view />
 </template>
 
@@ -26,5 +47,8 @@ nav {
       color: #42b983;
     }
   }
+}
+.v-toolbar-title__placeholder {
+  width: fit-content;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,27 @@
 <template>
-  <div class="home">
-    <h1 class="text font-weight-light">This is Home Page</h1>
-    <div><p style="width: fit-content" class="bg-red py-5 px-10">Test</p></div>
-    <v-divider></v-divider>
-    <v-container>
-      <v-row>
-        <v-col cols="12">
-          <v-sheet class="bg-orange pa-10" rounded="xl" elevation="12">
-            <p>Test</p>
-          </v-sheet>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-btn id="myButton">Activator</v-btn>
-          <v-menu activator="#myButton" offset="10" location="right">
-            <v-list>
-              <v-list-item
-                v-for="(menu, index) in menus"
-                :key="index"
-                :title="menu.title"
-                :append-icon="menu.icon"
-              ></v-list-item>
-            </v-list>
-          </v-menu>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <h1 class="text font-weight-light">This is homepage</h1>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <!-- old way to create tooltip from default browser on hover  -->
+        <!-- <p title="my tooltip element">test</p> -->
+
+        <p title="my element">
+          Hossam ElBadry
+          <v-tooltip activator="parent" location="top"
+            >Hossam Ayman Hasan ElBadry</v-tooltip
+          >
+        </p>
+
+        <span>
+          <v-icon>mdi-delete</v-icon>
+          <v-tooltip activator="parent" location="bottom"
+            >Delete Item</v-tooltip
+          >
+        </span>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      menus: [
-        { title: "Profile", icon: "mdi-account-outline" },
-        { title: "Home", icon: "mdi-home-outline" },
-      ],
-    };
-  },
-};
-</script>
+
+<script></script>
