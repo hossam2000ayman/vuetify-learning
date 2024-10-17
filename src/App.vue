fee478563169fafe277e67ca0f17a38707150477
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-17 23:03:51 +0300

    vuetify-v-tabs-and-v-window

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,65 @@
 <template>
   <v-layout>
-    <v-app-bar
-      elevation="0"
-      style="border-bottom: 1px solid rgba(255, 255, 255, 0.2)"
-      theme="light"
-      color="blue"
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-spacer></v-spacer>
-      <v-app-bar-title style="color: white">My AppBar Title</v-app-bar-title>
-      <v-icon class="mr-4">mdi-magnify</v-icon>
-      <v-icon class="mr-4">mdi-heart</v-icon>
-      <v-icon>mdi-dots-vertical</v-icon>
-    </v-app-bar>
+    <v-card class="w-100">
+      <!-- v-tabs is container of tabs -->
 
-    <v-app-bar elevation="2" color="blue">
-      <!-- <v-breadcrumbs :items="items" divider=">>"></v-breadcrumbs> -->
-      <v-breadcrumbs color="white" :items="items" divider="-">
-        <!-- If you need to make override on props then you can make it through template have v-slot:<name-of-props> -->
-        <!-- so this will override the divider props in the parent component (v-breadcrumbs) -->
-        <template v-slot:divider>
-          <v-icon color="white">mdi-chevron-right</v-icon>
-        </template>
-      </v-breadcrumbs>
-    </v-app-bar>
+      <v-tabs
+        v-model="tab"
+        prev-icon="mdi-arrow-left"
+        next-icon="mdi-arrow-right"
+      >
+        <v-tab variant="outlined" v-for="num in 20" :key="num" :value="num">
+          <v-icon>mdi-home</v-icon>
+          Tab {{ num }}
+        </v-tab>
+      </v-tabs>
+      <!-- <v-tabs v-model="tab" fixed-tabs>
+        <v-tab v-for="num in 3" :key="num" :value="num">
+          <v-icon>mdi-home</v-icon>
+          Tab {{ num }}
+        </v-tab>
+      </v-tabs> -->
 
-    <v-system-bar color="purple" elevation="10">
-      <v-icon class="mr-2">mdi-signal</v-icon>
-      <v-icon class="mr-2">mdi-wifi-strength-3</v-icon>
-      <v-icon class="mr-2">mdi-battery</v-icon>
-      <span>08:00 PM</span>
-    </v-system-bar>
+      <!-- <v-tabs v-model="tab" bg-color="orange">
+        <v-tab v-for="num in 20" :key="num" :value="num">
+          <v-icon>mdi-home</v-icon>
+          Tab {{ num }}
+        </v-tab>
+      </v-tabs> -->
+      <!-- <v-tabs v-model="tab" center-active>
+        <v-tab v-for="num in 20" :key="num" :value="num">
+          <v-icon>mdi-home</v-icon>
+          Tab {{ num }}
+        </v-tab>
+      </v-tabs> -->
+      <!-- <v-tabs v-model="tab" align-tabs="center">
+        <v-tab v-for="num in 20" :key="num" :value="num">
+          <v-icon>mdi-home</v-icon>
+          Tab {{ num }}
+        </v-tab>
+      </v-tabs> -->
 
-    <v-footer class="bg-green flex-column" elevation="20" :app="true">
-      <p style="color: white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt enim
-        iure molestiae sequi dicta eveniet quis dolorem. Minima, esse eius
-        adipisci dicta provident explicabo autem sapiente impedit ullam libero
-        sunt!
-      </p>
-      <v-container>
-        <v-icon color="white">mdi-home</v-icon>
-        <v-icon color="white">mdi-twitter</v-icon>
-        <v-icon color="white">mdi-facebook</v-icon>
-        <v-icon color="white">mdi-instagram</v-icon>
-      </v-container>
-    </v-footer>
+      <!-- <v-tabs v-model="tab" stacked>
+        <v-tab v-for="num in 4" :key="num" :value="num">
+          <v-icon>mdi-home</v-icon>
+          Tab {{ num }}
+        </v-tab>
+      </v-tabs> -->
+      <!-- to map the content with it's tab we need to add component -->
+      <!-- called window which parent of list of window-items  -->
+      <v-window v-model="tab" class="mt-5">
+        <v-window-item v-for="num in 20" :key="num" :value="num">
+          <v-card>
+            <v-title>card {{ num }} </v-title>
+          </v-card>
+        </v-window-item>
+      </v-window>
+    </v-card>
   </v-layout>
-  <router-view />
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const items = ref(["test 1", "test 2", "test 3"]);
+const tab = ref("");
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-.v-toolbar-title__placeholder {
-  width: fit-content;
-}
-</style>
