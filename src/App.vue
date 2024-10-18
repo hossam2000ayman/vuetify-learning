c312f49b8045aa5b64afbddad6355f6a217978bc
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-18 12:23:52 +0300

    vuetify-text-area-and-v-text-field

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,54 @@
 <template>
   <v-layout>
-    <v-card min-width="100%" min-height="50vh" class="pa-10" variant="outlined">
-      <v-card-title v-if="selectedGroup"> {{ selectedGroup }}</v-card-title>
-      <v-select
-        :items="names"
+    <v-card
+      min-width="100%"
+      min-height="50vh"
+      class="pa-10 text-center"
+      variant="outlined"
+    >
+      {{ myInput }}
+      <v-text-field
         variant="outlined"
-        v-model="selectedGroup"
+        type="email"
+        v-model="myInput"
+        label="Enter your email"
         :error-messages="errorMessage_1"
-        autofocus
-        multiple
-        item-title="text"
-        item-value="number"
-      ></v-select>
+      ></v-text-field>
 
-      <v-card-title v-if="selectedName"> {{ selectedName }}</v-card-title>
-      <v-select
-        :items="names"
+      <v-textarea
         variant="outlined"
-        v-model="selectedName"
+        no-resize
+        rows="10"
+        label="Enter your notes"
+        v-model="myTextAreaInput"
         :error-messages="errorMessage_2"
-        autofocus
-        item-title="text"
-        item-value="number"
-        return-object
-      ></v-select>
-      <v-btn @click="validate">Submit</v-btn>
+      ></v-textarea>
+      <v-btn @click="validate" class="mt-3">Validate</v-btn>
     </v-card>
   </v-layout>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const selectedName = ref(null);
-const selectedGroup = ref(null);
+
+const myInput = ref(null);
+const myTextAreaInput = ref(null);
+
 const errorMessage_1 = ref(null);
 const errorMessage_2 = ref(null);
-const names = ref([
-  //title , value can we defined by component by default not other key or you have to define it explicity instead for implicity way
-  //in case if you want to define it into v-select or any form component by item-title
-  {
-    text: "_Ahmed",
-    title: "Ahmed",
-    number: "_1",
-    value: 1,
-  },
-  {
-    text: "_Hossam",
-    title: "Hossam",
-    number: "_2",
-    value: 2,
-  },
-  {
-    text: "_Moaz",
-    title: "Moaz",
-    number: "_3",
-    value: 3,
-  },
-  {
-    text: "_Mahmoud",
-    title: "Mahmoud",
-    number: "_4",
-    value: 4,
-  },
-  {
-    text: "_Metwaly",
-    title: "Metwaly",
-    number: "_5",
-    value: 5,
-  },
-  {
-    text: "_Ayman",
-    title: "Ayman",
-    number: "_6",
-    value: 6,
-  },
-  {
-    text: "_Samer",
-    title: "Samer",
-    number: "_7",
-    value: 7,
-  },
-]);
 
 const validate = () => {
   errorMessage_1.value = "";
   errorMessage_2.value = "";
-  if (!selectedGroup.value || selectedGroup.value.length === 0) {
-    errorMessage_1.value = "برجاء اختار الاسم";
-  } else if (selectedGroup.value.length <= 1) {
-    errorMessage_1.value = "برجاء اختيار اكتر من مستخدم واحد";
+  if (!myInput.value) {
+    errorMessage_1.value = "please enter your email";
+  } else if (!myInput.value.includes("@") || !myInput.value.includes(".com")) {
+    errorMessage_1.value = "please enter valid email";
   }
-  if (!selectedName.value) {
-    errorMessage_2.value = "برجاء اختار الاسم";
+  if (!myTextAreaInput.value) {
+    errorMessage_2.value = "please enter your notes";
+  } else if (myTextAreaInput.value.length <= 10) {
+    errorMessage_2.value = "please enter at least 10 character";
   }
 };
 </script>
