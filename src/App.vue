90d8d7f03fbd32b65441d09a7c5a25820febb236
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-18 16:11:44 +0300

    vuetify-v-slider

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <v-layout>
     <v-card min-width="100%" class="pa-5" min-height="90vh" variant="outlined">
-      {{ imageFile }}
-      <v-file-input
-        multiple
-        v-model:model-value="imageFile"
-        @update:model-value="renderImage"
-        chips
-        variant="outlined"
-      ></v-file-input>
-
-      <v-card>
-        <img :src="imageUrl" alt="" class="pl-10" width="600px" />
-      </v-card>
+      {{ range }}
+      <v-slider
+        :min="0"
+        :max="10"
+        v-model="range"
+        step="0.25"
+        color="red"
+        track-color="blue"
+        thumb-label="always"
+        thumb-size="30"
+        class="mt-6"
+        append-icon="mdi-plus"
+        prepend-icon="mdi-minus"
+        @click:append="increment"
+        @click:prepend="decrement"
+      ></v-slider>
     </v-card>
   </v-layout>
 </template>
@@ -20,22 +24,14 @@
 <script setup>
 import { ref } from "vue";
 
-const imageFile = ref("");
-const imageUrl = ref("");
+const range = ref(0);
 
-const renderImage = () => {
-  if (!imageFile.value[0]) {
-    imageUrl.value = "";
-    return;
-  }
-  console.log("validated");
-  const file = imageFile.value[0];
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.addEventListener("load", () => {
-    console.log("File Reader :: ", reader.result);
-    imageUrl.value = reader.result;
-  });
+const decrement = () => {
+  range.value -= 0.25;
+};
+
+const increment = () => {
+  range.value += 0.25;
 };
 </script>
 <style scoped></style>
