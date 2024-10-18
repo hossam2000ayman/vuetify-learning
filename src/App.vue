a3e7681f9eb822309249ff2c2e9268ff337fd165
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-18 17:26:49 +0300

    vuetify-v-table

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,254 @@
 <template>
-  <v-layout>
-    <v-app-bar :theme="switchTheme"></v-app-bar>
-    <v-card
-      min-width="100%"
-      class="pa-5 mt-10"
-      min-height="90vh"
-      variant="outlined"
-    >
-      <v-switch
-        :label="`Theme: ${switchTheme}`"
-        v-model="switchTheme"
-        false-value="light"
-        true-value="dark"
-      ></v-switch>
+  <div class="d-flex align-center flex-column pa-6">
+    <v-card width="70%">
+      <v-table
+        height="200"
+        hover
+        density="compact"
+        fixed-header
+        fixed-footer
+        theme="dark"
+      >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Favorite Sport</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in items" :key="item.name">
+            <td>{{ item.name }}</td>
+            <td>{{ item.age }}</td>
+            <td>{{ item.favoriteSport }}</td>
+            <td>{{ item.country }}</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td class="bg-white">Table Data</td>
+            <td class="bg-white" colspan="3">Users</td>
+          </tr>
+        </tfoot>
+      </v-table>
     </v-card>
-
-    <v-footer app class="pa-5" :theme="switchTheme">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis error
-        sit ad iure! Nulla cum voluptate inventore impedit, vel esse beatae
-        quibusdam rerum, unde voluptatem explicabo eveniet nihil, asperiores
-        iure!
-      </p>
-    </v-footer>
-  </v-layout>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const switchTheme = ref("light");
+const items = ref([
+  {
+    name: "Hossam",
+    age: 24,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Mahmoud",
+    age: 27,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Moaz",
+    age: 22,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Salem",
+    age: 21,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "John",
+    age: 20,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Hossam",
+    age: 24,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Mahmoud",
+    age: 27,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Moaz",
+    age: 22,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Salem",
+    age: 21,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "John",
+    age: 20,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Hossam",
+    age: 24,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Mahmoud",
+    age: 27,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Moaz",
+    age: 22,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Salem",
+    age: 21,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "John",
+    age: 20,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Hossam",
+    age: 24,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Mahmoud",
+    age: 27,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Moaz",
+    age: 22,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Salem",
+    age: 21,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "John",
+    age: 20,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Hossam",
+    age: 24,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Mahmoud",
+    age: 27,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Moaz",
+    age: 22,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Salem",
+    age: 21,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "John",
+    age: 20,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Hossam",
+    age: 24,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Mahmoud",
+    age: 27,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Moaz",
+    age: 22,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "Salem",
+    age: 21,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+
+  {
+    name: "John",
+    age: 20,
+    favoriteSport: "football",
+    country: "Egypt",
+  },
+]);
 </script>
 <style scoped></style>
