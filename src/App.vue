434ea7c269f16fdbf0efa8ede381da7d651d57fd
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-18 10:15:49 +0300

    vuetify-v-autocomplete

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,21 @@
 <template>
   <v-layout>
-    <v-app-bar color="blue">
-      <v-app-bar-nav-icon @click="isDrawer = !isDrawer"></v-app-bar-nav-icon>
-    </v-app-bar>
     <v-card min-width="100%">
-      <v-navigation-drawer color="white" permanent rail expand-on-hover>
-        <v-list nav>
-          <v-list-item
-            class="text-start"
-            v-for="num in 5"
-            :key="num"
-            :value="num"
-            prepend-icon="mdi-home"
-            @click="console.log('clicked List', num)"
-          >
-            <v-list-item-title> List {{ num }} </v-list-item-title>
-          </v-list-item>
-          <v-list-group>
-            <template v-slot:activator="{ props }">
-              <v-list-item prepend-icon="mdi-account" v-bind="props">
-                <v-list-item-title>Test</v-list-item-title>
-              </v-list-item>
-            </template>
-            <v-list-item v-for="num in 8" :key="num">
-              <v-list-item-title> Test {{ num }} </v-list-item-title>
-            </v-list-item>
-          </v-list-group>
-        </v-list>
-      </v-navigation-drawer>
+      <v-card-title v-if="searchTxt">
+        Search Text :: {{ searchTxt }}
+      </v-card-title>
+      <v-card-title v-if="writeTxt">
+        Write Text :: {{ writeTxt }}
+      </v-card-title>
+      <v-autocomplete
+        :items="items"
+        v-model="searchTxt"
+        v-model:search="writeTxt"
+        no-data-text="لا يوجد بيانات"
+        variant="outlined"
+        :error-messages="errorMessage"
+      ></v-autocomplete>
+      <v-btn @click="validate">Submit</v-btn>
     </v-card>
   </v-layout>
 </template>
@@ -35,11 +23,25 @@
 <script setup>
 import { ref } from "vue";
 
-const isDrawer = ref(false);
+const searchTxt = ref(null);
+const writeTxt = ref(null);
+const errorMessage = ref(null);
+const items = ref([
+  "Ahmed",
+  "Ayman",
+  "Mohamed",
+  "Hossam",
+  "Moaz",
+  "Mahmoud",
+  "Amr",
+  "Abdullah",
+]);
+
+const validate = () => {
+  errorMessage.value = "";
+  if (!searchTxt.value) {
+    errorMessage.value = "برجاء ادخال الاسم ";
+  }
+};
 </script>
-<style scoped>
-.v-list .v-list-item--nav:not(:only-child) {
-  display: flex;
-  justify-content: flex-start;
-}
-</style>
+<style scoped></style>
