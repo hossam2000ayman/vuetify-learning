1abd7e63bd6055dd23bff752edf7495c9617e0e3
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-18 11:45:17 +0300

    vuetify-v-select

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <v-layout>
-    <v-card min-width="100%">
-      <v-card-title v-if="searchTxt">
-        Search Text :: {{ searchTxt }}
-      </v-card-title>
-      <v-card-title v-if="writeTxt">
-        Write Text :: {{ writeTxt }}
-      </v-card-title>
-      <v-autocomplete
-        :items="items"
-        v-model="searchTxt"
-        v-model:search="writeTxt"
-        no-data-text="لا يوجد بيانات"
+    <v-card min-width="100%" min-height="50vh" class="pa-10" variant="outlined">
+      <v-card-title v-if="selectedGroup"> {{ selectedGroup }}</v-card-title>
+      <v-select
+        :items="names"
         variant="outlined"
-        :error-messages="errorMessage"
-      ></v-autocomplete>
+        v-model="selectedGroup"
+        :error-messages="errorMessage_1"
+        autofocus
+        multiple
+        item-title="text"
+        item-value="number"
+      ></v-select>
+
+      <v-card-title v-if="selectedName"> {{ selectedName }}</v-card-title>
+      <v-select
+        :items="names"
+        variant="outlined"
+        v-model="selectedName"
+        :error-messages="errorMessage_2"
+        autofocus
+        item-title="text"
+        item-value="number"
+        return-object
+      ></v-select>
       <v-btn @click="validate">Submit</v-btn>
     </v-card>
   </v-layout>
@@ -22,25 +31,67 @@
 
 <script setup>
 import { ref } from "vue";
-
-const searchTxt = ref(null);
-const writeTxt = ref(null);
-const errorMessage = ref(null);
-const items = ref([
-  "Ahmed",
-  "Ayman",
-  "Mohamed",
-  "Hossam",
-  "Moaz",
-  "Mahmoud",
-  "Amr",
-  "Abdullah",
+const selectedName = ref(null);
+const selectedGroup = ref(null);
+const errorMessage_1 = ref(null);
+const errorMessage_2 = ref(null);
+const names = ref([
+  //title , value can we defined by component by default not other key or you have to define it explicity instead for implicity way
+  //in case if you want to define it into v-select or any form component by item-title
+  {
+    text: "_Ahmed",
+    title: "Ahmed",
+    number: "_1",
+    value: 1,
+  },
+  {
+    text: "_Hossam",
+    title: "Hossam",
+    number: "_2",
+    value: 2,
+  },
+  {
+    text: "_Moaz",
+    title: "Moaz",
+    number: "_3",
+    value: 3,
+  },
+  {
+    text: "_Mahmoud",
+    title: "Mahmoud",
+    number: "_4",
+    value: 4,
+  },
+  {
+    text: "_Metwaly",
+    title: "Metwaly",
+    number: "_5",
+    value: 5,
+  },
+  {
+    text: "_Ayman",
+    title: "Ayman",
+    number: "_6",
+    value: 6,
+  },
+  {
+    text: "_Samer",
+    title: "Samer",
+    number: "_7",
+    value: 7,
+  },
 ]);
 
 const validate = () => {
-  errorMessage.value = "";
-  if (!searchTxt.value) {
-    errorMessage.value = "برجاء ادخال الاسم ";
+  errorMessage_1.value = "";
+  errorMessage_2.value = "";
+  if (!selectedGroup.value || selectedGroup.value.length === 0) {
+    errorMessage_1.value = "برجاء اختار الاسم";
+  } else if (selectedGroup.value.length <= 1) {
+    errorMessage_1.value = "برجاء اختيار اكتر من مستخدم واحد";
+  }
+  if (!selectedName.value) {
+    errorMessage_2.value = "برجاء اختار الاسم";
   }
 };
 </script>
