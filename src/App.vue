992c318763096427dd55fdee18ebbac3c8ef6b90
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-19 14:53:17 +0300

    vueitify-v-btn-toggle

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,107 @@
 <template>
-  <v-layout>
-    <v-card min-width="100%" min-height="50vh" class="pa-10" variant="outlined">
-      <v-card-title v-if="selectedGroup"> {{ selectedGroup }}</v-card-title>
-      <v-select
-        :items="names"
-        variant="outlined"
-        v-model="selectedGroup"
-        :error-messages="errorMessage_1"
-        autofocus
-        multiple
-        item-title="text"
-        item-value="number"
-      ></v-select>
-
-      <v-card-title v-if="selectedName"> {{ selectedName }}</v-card-title>
-      <v-select
-        :items="names"
-        variant="outlined"
-        v-model="selectedName"
-        :error-messages="errorMessage_2"
-        autofocus
-        item-title="text"
-        item-value="number"
-        return-object
-      ></v-select>
-      <v-btn @click="validate">Submit</v-btn>
+  <div v-if="false" class="d-flex align-center flex-column pa-6">
+    <v-card width="70%">
+      <v-card-title>
+        Normal Selected Toggle Button :: {{ selectedButton_1 }}</v-card-title
+      >
+      <v-btn-toggle v-model="selectedButton_1">
+        <v-btn value="_home"><v-icon>mdi-home</v-icon></v-btn>
+        <v-btn value="_account"><v-icon>mdi-account</v-icon></v-btn>
+        <v-btn value="_file"><v-icon>mdi-file-document</v-icon></v-btn>
+        <v-btn value="_music"><v-icon>mdi-music</v-icon></v-btn>
+      </v-btn-toggle>
     </v-card>
-  </v-layout>
+
+    <v-card width="70%">
+      <v-card-title>
+        Mandatory Selected Toggle Button :: {{ selectedButton_2 }}</v-card-title
+      >
+      <v-btn-toggle v-model="selectedButton_2" mandatory color="primary">
+        <v-btn value="_home"><v-icon>mdi-home</v-icon></v-btn>
+        <v-btn value="_account"><v-icon>mdi-account</v-icon></v-btn>
+        <v-btn value="_file"><v-icon>mdi-file-document</v-icon></v-btn>
+        <v-btn value="_music"><v-icon>mdi-music</v-icon></v-btn>
+      </v-btn-toggle>
+    </v-card>
+
+    <v-card width="70%">
+      <v-card-title>
+        Multiple Selected Toggle Button :: {{ selectedButton_3 }}</v-card-title
+      >
+      <v-btn-toggle v-model="selectedButton_3" multiple color="primary">
+        <v-btn value="_home"><v-icon>mdi-home</v-icon></v-btn>
+        <v-btn value="_account"><v-icon>mdi-account</v-icon></v-btn>
+        <v-btn value="_file"><v-icon>mdi-file-document</v-icon></v-btn>
+        <v-btn value="_music"><v-icon>mdi-music</v-icon></v-btn>
+      </v-btn-toggle>
+    </v-card>
+
+    <v-card width="70%" class="pa-5">
+      <v-card-title>
+        Divided Outlined Elevated Selected Toggle Button ::
+        {{ selectedButton_1 }}</v-card-title
+      >
+      <v-btn-toggle
+        v-model="selectedButton_1"
+        divided
+        variant="outlined"
+        elevation="10"
+        color="primary"
+      >
+        <v-btn value="_home"><v-icon>mdi-home</v-icon></v-btn>
+        <v-btn value="_account"><v-icon>mdi-account</v-icon></v-btn>
+        <v-btn value="_file"><v-icon>mdi-file-document</v-icon></v-btn>
+        <v-btn value="_music"><v-icon>mdi-music</v-icon></v-btn>
+      </v-btn-toggle>
+    </v-card>
+  </div>
+
+  <div class="d-flex align-center flex-column pa-6">
+    <v-card width="70%" class="pa-5 justify-space-around d-flex">
+      <!-- Font Style Button Toggle -->
+      <v-btn-toggle
+        v-model="toggleStyle"
+        multiple
+        color="primary"
+        divided
+        variant="outlined"
+      >
+        <v-btn value="bold"><v-icon>mdi-format-bold</v-icon></v-btn>
+        <v-btn value="italic"><v-icon>mdi-format-italic</v-icon></v-btn>
+        <v-btn value="underline"><v-icon>mdi-format-underline</v-icon></v-btn>
+      </v-btn-toggle>
+    </v-card>
+
+    <!-- Textarea where style is applied -->
+    <v-card width="70%" class="pa-5">
+      <v-textarea
+        rows="10"
+        variant="outlined"
+        :class="['w-100', 'pa-5', `font-weight-${boldStyle}`, 'text-center']"
+        :style="`border: 1px solid; font-style: ${italicStyle}; text-decoration: ${underlineStyle}`"
+      ></v-textarea>
+    </v-card>
+  </div>
 </template>
 
 <script setup>
+import { computed } from "vue";
 import { ref } from "vue";
-const selectedName = ref(null);
-const selectedGroup = ref(null);
-const errorMessage_1 = ref(null);
-const errorMessage_2 = ref(null);
-const names = ref([
-  //title , value can we defined by component by default not other key or you have to define it explicity instead for implicity way
-  //in case if you want to define it into v-select or any form component by item-title
-  {
-    text: "_Ahmed",
-    title: "Ahmed",
-    number: "_1",
-    value: 1,
-  },
-  {
-    text: "_Hossam",
-    title: "Hossam",
-    number: "_2",
-    value: 2,
-  },
-  {
-    text: "_Moaz",
-    title: "Moaz",
-    number: "_3",
-    value: 3,
-  },
-  {
-    text: "_Mahmoud",
-    title: "Mahmoud",
-    number: "_4",
-    value: 4,
-  },
-  {
-    text: "_Metwaly",
-    title: "Metwaly",
-    number: "_5",
-    value: 5,
-  },
-  {
-    text: "_Ayman",
-    title: "Ayman",
-    number: "_6",
-    value: 6,
-  },
-  {
-    text: "_Samer",
-    title: "Samer",
-    number: "_7",
-    value: 7,
-  },
-]);
 
-const validate = () => {
-  errorMessage_1.value = "";
-  errorMessage_2.value = "";
-  if (!selectedGroup.value || selectedGroup.value.length === 0) {
-    errorMessage_1.value = "برجاء اختار الاسم";
-  } else if (selectedGroup.value.length <= 1) {
-    errorMessage_1.value = "برجاء اختيار اكتر من مستخدم واحد";
-  }
-  if (!selectedName.value) {
-    errorMessage_2.value = "برجاء اختار الاسم";
-  }
-};
+const selectedButton_1 = ref("_home");
+const selectedButton_2 = ref("_music");
+const selectedButton_3 = ref(["_file"]);
+// const toggleAlign = ref("start");
+const toggleStyle = ref([]);
+
+const boldStyle = computed(() => {
+  return toggleStyle.value.filter((style) => style === "bold").join();
+});
+const italicStyle = computed(() => {
+  return toggleStyle.value.filter((style) => style === "italic").join();
+});
+const underlineStyle = computed(() => {
+  return toggleStyle.value.filter((style) => style === "underline").join();
+});
 </script>
 <style scoped></style>
