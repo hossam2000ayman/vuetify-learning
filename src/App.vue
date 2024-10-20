8510937f6f2c3ea2381daf145e09c99eccb7d56d
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-21 00:18:25 +0300

    vuetify-v-slide-group

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,37 @@
 <template>
   <div class="d-flex align-center flex-column pa-6">
     <v-card width="70%">
-      <v-carousel
-        height="200"
-        show-arrows="hover"
-        cycle
-        interval="2000"
-        delimiter-icon="mdi-home"
-        hide-delimiter-background
-        progress
-      >
-        <template v-slot:prev="{ props }">
-          <v-btn v-bind="props" color="green"
-            ><v-icon color="yellow">mdi-arrow-left</v-icon></v-btn
+      <v-slide-group v-model="selectedItem" mandatory>
+        <v-slide-group-item
+          v-for="num in 25"
+          :key="num"
+          v-slot="{ isSelected, toggle }"
+          :value="`Item : ${num}`"
+        >
+          <v-card
+            width="200"
+            class="mr-3"
+            @click="toggle"
+            v-bind="isSelected"
+            :color="isSelected ? 'primary' : ''"
           >
-        </template>
-
-        <template v-slot:next="{ props }">
-          <v-btn v-bind="props" color="green"
-            ><v-icon color="yellow">mdi-arrow-right</v-icon></v-btn
-          >
-        </template>
-        <v-carousel-item
-          cover
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-        ></v-carousel-item>
-        <v-carousel-item
-          cover
-          src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
-        ></v-carousel-item>
-        <v-carousel-item
-          cover
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-        ></v-carousel-item>
-      </v-carousel>
+            <v-card-title>Card Number : {{ num }} </v-card-title>
+            <v-card-text>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              quis odio dolorem voluptatem vero harum dicta fuga ipsa maxime.
+              Totam pariatur nostrum asperiores culpa similique, dignissimos
+              earum aperiam necessitatibus. Praesentium?
+            </v-card-text>
+          </v-card>
+        </v-slide-group-item>
+      </v-slide-group>
+      {{ selectedItem }}
     </v-card>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const selectedItem = ref("");
+</script>
