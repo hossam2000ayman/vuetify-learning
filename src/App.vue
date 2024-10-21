bb22697c08a9976061e00f960b2308ec6623f0e7
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-22 00:08:35 +0300

    vuetify-v-badge

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,29 @@
 <template>
   <div class="d-flex align-center flex-column pa-6">
-    <v-card width="70%" class="pa-5" height="560">
-      <v-btn @click="(alert = true), (alertType = 'success')"
-        >Show Success</v-btn
-      >
-      <v-btn @click="(alert = true), (alertType = 'info')">Show Info</v-btn>
-      <v-btn @click="(alert = true), (alertType = 'warning')"
-        >Show Warning</v-btn
-      >
-      <v-btn @click="(alert = true), (alertType = 'error')">Show Error</v-btn>
-
-      <v-alert
-        :color="alertType"
-        :type="alertType"
-        :title="alertType"
-        closable
-        border="start"
-        height="220"
-        v-model="alert"
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nesciunt
-        magni quas tempora neque deleniti laborum aperiam, aspernatur,
-        consectetur quis maiores! Dolore necessitatibus at molestiae ex iste
-        eaque pariatur vel?
-      </v-alert>
+    <v-card width="70%" class="pa-5 text-center" height="560">
+      <v-btn @click="showBadge = !showBadge">Show Badge</v-btn>
+      <v-text-field type="number" v-model="badgeContent"></v-text-field>
+      <v-btn variant="text">
+        <v-badge
+          :content="badgeContent"
+          color="error"
+          offset-x="2"
+          offset-y="2"
+          bordered
+          location="top right"
+          max="18"
+          v-model="showBadge"
+        >
+          <v-icon size="30">mdi-bell</v-icon>
+        </v-badge>
+      </v-btn>
     </v-card>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const alert = ref(false);
-const alertType = ref("");
+
+const badgeContent = ref(0);
+const showBadge = ref(false);
 </script>
