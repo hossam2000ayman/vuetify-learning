c4c322c1ed7782f06559fff1c90fddb2a27004b3
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-22 00:09:37 +0300

    vuetify-v-hover

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
   <div class="d-flex align-center flex-column pa-6">
-    <v-card width="70%" class="pa-5 text-center" height="560">
-      <v-btn @click="showBadge = !showBadge">Show Badge</v-btn>
-      <v-text-field type="number" v-model="badgeContent"></v-text-field>
-      <v-btn variant="text">
-        <v-badge
-          :content="badgeContent"
-          color="error"
-          offset-x="2"
-          offset-y="2"
-          bordered
-          location="top right"
-          max="18"
-          v-model="showBadge"
-        >
-          <v-icon size="30">mdi-bell</v-icon>
-        </v-badge>
-      </v-btn>
-    </v-card>
+    <v-hover v-slot="{ isHovering, props }" open-delay="300" close-delay="400">
+      <v-card
+        width="70%"
+        class="pa-5 text-center"
+        height="300"
+        v-bind="props"
+        :elevation="isHovering ? 10 : 1"
+        :color="isHovering ? 'red' : ''"
+      >
+        <v-card-title> This is title </v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nesciunt
+          magni quas tempora neque deleniti laborum aperiam, aspernatur,
+          consectetur quis maiores! Dolore necessitatibus at molestiae ex iste
+          eaque pariatur vel?
+        </v-card-text>
+      </v-card>
+    </v-hover>
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-
-const badgeContent = ref(0);
-const showBadge = ref(false);
-</script>
+<script setup></script>
