d20d19471b6268889f22b715b6c0df1458d21cf8
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-22 00:01:12 +0300

    vuetify-v-window

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="d-flex align-center flex-column pa-6">
-    <v-card width="70%">
-      <v-chip-group v-model="sports" multiple color="red" variant="outlined">
-        <v-chip value="Football" filter filter-icon="mdi-account">
-          Football
-        </v-chip>
-        <v-chip value="Tennis" filter>Tennis</v-chip>
-        <v-chip value="Basketball" filter>Basketball</v-chip>
-      </v-chip-group>
-      {{ sports }}
+    <v-card width="70%" class="pa-5">
+      <v-window
+        class="bg-blue"
+        show-arrows
+        prev-icon="mdi-arrow-left"
+        next-icon="mdi-arrow-right"
+      >
+        <v-window-item v-for="n in 25" :key="n">
+          <v-card width="100%" color="green">
+            <v-card-title>This is Title {{ n }}</v-card-title>
+            <v-card-text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+              nesciunt magni quas tempora neque deleniti laborum aperiam,
+              aspernatur, consectetur quis maiores! Dolore necessitatibus at
+              molestiae ex iste eaque pariatur vel?
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+      </v-window>
     </v-card>
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-
-const sports = ref(["Football"]);
-</script>
+<script setup></script>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import { createVuetify } from "vuetify/lib/framework.mjs";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "vuetify/styles";
-// added
 import "@mdi/font/css/materialdesignicons.css";
 const vuetify = createVuetify({
   components,
