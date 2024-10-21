a1ab2212197f334dda2b9c521c82f3a71390c4f1
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-22 00:04:32 +0300

    vuetify-v-alert

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="d-flex align-center flex-column pa-6">
-    <v-card width="70%" class="pa-5">
-      <v-window
-        class="bg-blue"
-        show-arrows
-        prev-icon="mdi-arrow-left"
-        next-icon="mdi-arrow-right"
+    <v-card width="70%" class="pa-5" height="560">
+      <v-btn @click="(alert = true), (alertType = 'success')"
+        >Show Success</v-btn
       >
-        <v-window-item v-for="n in 25" :key="n">
-          <v-card width="100%" color="green">
-            <v-card-title>This is Title {{ n }}</v-card-title>
-            <v-card-text>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-              nesciunt magni quas tempora neque deleniti laborum aperiam,
-              aspernatur, consectetur quis maiores! Dolore necessitatibus at
-              molestiae ex iste eaque pariatur vel?
-            </v-card-text>
-          </v-card>
-        </v-window-item>
-      </v-window>
+      <v-btn @click="(alert = true), (alertType = 'info')">Show Info</v-btn>
+      <v-btn @click="(alert = true), (alertType = 'warning')"
+        >Show Warning</v-btn
+      >
+      <v-btn @click="(alert = true), (alertType = 'error')">Show Error</v-btn>
+
+      <v-alert
+        :color="alertType"
+        :type="alertType"
+        :title="alertType"
+        closable
+        border="start"
+        height="220"
+        v-model="alert"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nesciunt
+        magni quas tempora neque deleniti laborum aperiam, aspernatur,
+        consectetur quis maiores! Dolore necessitatibus at molestiae ex iste
+        eaque pariatur vel?
+      </v-alert>
     </v-card>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const alert = ref(false);
+const alertType = ref("");
+</script>
