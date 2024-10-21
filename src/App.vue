ccfe7edbc749672959ee9532ed088a2b758ef787
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-21 09:47:23 +0300

    vuetify-v-chip-group

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
 <template>
   <div class="d-flex align-center flex-column pa-6">
     <v-card width="70%">
-      <v-slide-group v-model="selectedItem" mandatory>
-        <v-slide-group-item
-          v-for="num in 25"
-          :key="num"
-          v-slot="{ isSelected, toggle }"
-          :value="`Item : ${num}`"
-        >
-          <v-card
-            width="200"
-            class="mr-3"
-            @click="toggle"
-            v-bind="isSelected"
-            :color="isSelected ? 'primary' : ''"
-          >
-            <v-card-title>Card Number : {{ num }} </v-card-title>
-            <v-card-text>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
-              quis odio dolorem voluptatem vero harum dicta fuga ipsa maxime.
-              Totam pariatur nostrum asperiores culpa similique, dignissimos
-              earum aperiam necessitatibus. Praesentium?
-            </v-card-text>
-          </v-card>
-        </v-slide-group-item>
-      </v-slide-group>
-      {{ selectedItem }}
+      <v-chip-group v-model="sports" multiple color="red" variant="outlined">
+        <v-chip value="Football" filter filter-icon="mdi-account">
+          Football
+        </v-chip>
+        <v-chip value="Tennis" filter>Tennis</v-chip>
+        <v-chip value="Basketball" filter>Basketball</v-chip>
+      </v-chip-group>
+      {{ sports }}
     </v-card>
   </div>
 </template>
@@ -33,5 +16,5 @@
 <script setup>
 import { ref } from "vue";
 
-const selectedItem = ref("");
+const sports = ref(["Football"]);
 </script>
