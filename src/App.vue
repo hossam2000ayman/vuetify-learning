ea8e73dc4cd70229bd79969cdbc2f917ca754bbc
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-22 00:16:21 +0300

    vuetify-v-lazy

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="d-flex align-center flex-column pa-6">
-    <v-card max-width="500" width="500" height="200">
-      <v-avatar
-        color="black"
-        size="100"
-        rounded="0"
-        variant="outlined"
-        image="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
-      >
-      </v-avatar>
+    <v-card max-width="500" width="500" height="2000">
+      <v-lazy v-for="num in 100" :key="num" color="red" class="mt-10">
+        <v-card image="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg">
+          <v-card-title>this is card title</v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+            nesciunt magni quas tempora neque deleniti laborum aperiam,
+            aspernatur, consectetur quis maiores! Dolore necessitatibus at
+            molestiae ex iste eaque pariatur vel?
+          </v-card-text>
+        </v-card>
+      </v-lazy>
     </v-card>
   </div>
 </template>
-
 <script setup></script>
